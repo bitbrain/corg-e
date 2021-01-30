15dd0313e8cfc28b6f1180a05b744ebe8ba36493
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2021-01-30 15:40:21 +0000

    :sparkles: implement totem activation

--- godot/totem/Totem.gd
+++ godot/totem/Totem.gd
@@ -1,6 +1,8 @@
 class_name Totem
 extends StaticBody2D
 
+const MIN_GEM_DISTANCE = 3
+
 enum Type {
 	BASS,
 	TENOR,
@@ -13,12 +15,30 @@ export(Type) var type = Type.BASS
 onready var ysort = $YSort
 onready var sprite = $YSort/Sprite
 onready var gem_slot_position = $GemSlotPosition.global_position
+onready var animation_player = $TotemActivationPlayer
+onready var activation_line_sprite = $YSort/Sprite/ActivationLineSprite
+onready var face_sprite = $FaceActivationSprite
 
 var gem:Gem = null
 
+var sleeping = true
+
 func _ready():
 	sprite.frame = type
-
+	
+func _physics_process(delta):
+	if sleeping && gem != null:
+		var distance = gem.global_position.distance_to(gem_slot_position)
+		if distance < MIN_GEM_DISTANCE:
+			# WAKE UP!
+			gem.global_position = gem_slot_position
+			sleeping = false
+			activation_line_sprite.visible = true
+			face_sprite.visible = true
+			# TODO select animation based on type
+			animation_player.play("ActivateTotemBass")
+		else:
+			gem.global_position = gem.global_position.move_toward(gem_slot_position, distance * delta)
 
 func _on_GemDetectionArea_area_entered(area):
 	if area is Gem && self.gem == null && self.type == area.type:
@@ -32,3 +52,7 @@ func _on_GemDetectionArea_area_entered(area):
 		area.queue_free()
 		# set the correct global pos after reparenting
 		self.gem.global_position = old_global_pos
+		
+func _completely_woken_up():
+	# TODO select animation based on type
+	animation_player.play("SingingTotemBass")
