48783eb77faacff805b20a27617feb9a2a776941
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2021-01-30 19:29:04 +0000

    :bento: update alien design

--- godot/alien/Alien.gd
+++ godot/alien/Alien.gd
@@ -8,9 +8,22 @@ enum Type {
 	SOPRANO
 }
 
+const ANIMATION_EXTENSIONS = [
+	"Bass", "Tenor", "Alto", "Soprano"
+]
+const LIGHT_COLOURS = [
+	"ff4b98", # BASS
+	"90ff58", # TENOR
+	"ff5649", #ALTO
+	"59dfff"  # SOPRANO
+	]
+
 export(Type) var type = Type.BASS
 
 onready var sprite = $Sprite
+onready var animation_player = $AnimationPlayer
+onready var light = $Light2D
 
 func _ready():
-	sprite.frame = type
+	animation_player.play(ANIMATION_EXTENSIONS[type] + "Idle")
+	light.color = Color(LIGHT_COLOURS[type])	
