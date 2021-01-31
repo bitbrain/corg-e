25b810c5947dd0874a42a4dbdf2398629959224f
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2021-01-31 12:36:21 +0000

    implement alien sounds

---
+++ godot/audio/JukeBox.gd
@@ -0,0 +1,13 @@
+tool
+extends Node2D
+
+var random = RandomNumberGenerator.new()
+
+func play():
+	if get_child_count() < 1:
+		return
+	random.randomize()
+	var randomIndex = random.randi_range(0, get_child_count() - 1)
+	var player = get_child(randomIndex)
+	if player.has_method("play"):
+		player.play()
